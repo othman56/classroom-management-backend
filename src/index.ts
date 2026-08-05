f895ffa6cf3c10cb5a4dbe59d30cea5422643a7c
usman muhammad hassan <hassanusman5667@gmail.com>
2026-08-05 11:31:41 +0100

    fixed one file extension

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import subjectRouter from "./routes/subject.js";
 import cors from "cors";
 import securityMiddleware from "./middleware/security.js";
 import { toNodeHandler } from "better-auth/node";
-import { auth } from "./lib/auth";
+import { auth } from "./lib/auth.js";
 
 const app = express();
 const PORT = 8000;
