1c93c211f6caa3ddb4fc9c42736f025fdbbfdab5
usman muhammad hassan <hassanusman5667@gmail.com>
2026-08-05 11:28:19 +0100

    added more file extensions

--- src/db/schema/app.ts
+++ src/db/schema/app.ts
@@ -10,7 +10,7 @@ import {
   varchar,
 } from "drizzle-orm/pg-core";
 import { relations } from "drizzle-orm/relations";
-import { user } from "./auth";
+import { user } from "./auth.js";
 
 export const classStatusEnum = pgEnum("class_status", [
   "active",
